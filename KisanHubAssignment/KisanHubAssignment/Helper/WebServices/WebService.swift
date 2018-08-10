dec78c3ecb2d1491d3215d74608dbe83784f1a73
Ashwinkumar Mangrulkar <amangrulkar22@gmail.com>
2018-08-10 16:35:20 +0530

    Webservice class changes

---
+++ KisanHubAssignment/KisanHubAssignment/Helper/WebServices/WebService.swift
@@ -0,0 +1,57 @@
+//
+//  WebService.swift
+//  KisanHubAssignment
+//
+//  Created by Ashwinkumar Mangrulkar on 10/08/18.
+//  Copyright © 2018 Ashwinkumar Mangrulkar. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import Reachability
+
+class WebService {
+    
+    static let sharedInstance = WebService()
+    
+    /// Function to check Imternet Connection available or not
+    ///
+    /// - Returns: return bool value
+    func isInternetAvailable()->Bool
+    {
+        //declare this property where it won't go out of scope relative to your listener
+        let reachability = Reachability()!
+        
+        if reachability.connection != .none {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    /// Web Servce for getting map data
+    ///
+    ///   - withCompletionHandler: Response
+    func webServiceGetMapData(_ CompletionHandler:@escaping (_ success:Bool, _ responseDictionary:AnyObject?, _ error:NSError?)->Void)
+    {
+        //--Checking internet
+        if isInternetAvailable()==false
+        {
+            CompletionHandler(false, nil, nil)
+            return
+        }
+
+        print("url \(Url_Google_Map)")
+        
+//        Alamofire.request(Url_Google_Map, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil).responseJSON { (response) in
+        Alamofire.request(Url_Google_Map).responseJSON { (response) in
+            if let JSON = response.result.value {
+                CompletionHandler(true, JSON as AnyObject?,response.result.error as NSError?)
+            }else
+            {
+                CompletionHandler(false, nil,response.result.error as NSError?)
+            }
+        }
+    }
+    
+}
